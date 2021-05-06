6e8ceef83a1647c29e68e3534202d9e71ad187a1
Vorpalsword-yt <79005655+Vorpalsword-yt@users.noreply.github.com>
2021-05-06 15:13:53 +0300

    Change Language on English

--- consts.py
+++ consts.py
@@ -1,24 +1,24 @@
 # Messages
 START_MESSAGE = """
-*Бот для поиска ваканский*
+*Bot for job search*
 """
 EMPLOYER_MESSAGE = """
-Вы находитель в меню заполнения заявки
-Воспользуйтесь клавиатурой для ввода необходимых данных
+This is menu which you should fill
+Please, use the keyboard to enter the required data
 """
 
 # KBs
 START_KB = {
-    "text": ["Поиск в боте", "Поиск в онлайн", "Работодателю", "Сменить локацию"],
+    "text": ["Search in the Bot", "Online search", "For the employer", "Change location"],
     "callbacks": ["search_bot", "search_online", "employer", "ch_location"],
 }
 
 EMPLOYER_KB = {
-    "text": ["Название", "Информация", "Локация", "Контакты", "Назад"],
+    "text": ["Title", "Information", "Location", "Contacts", "Back"],
     "callbacks": ["set_title", "set_info", "set_location", "set_contacts", "back_{page}"],
 }
 
 BACK_KB = {
-    "text": ["Назад"],
+    "text": ["Back"],
     "callbacks": ["back_{page}"]
 }
